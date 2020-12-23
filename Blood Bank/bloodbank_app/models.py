468c9224f9f1e0caa64841385d56e6b26d9c6512
Sk-Kamal-Hossain <skkamal6301@gmail.com>
2020-12-23 22:36:45 +0600

    Try to conenct database but somehow I cannot conenct to the database

--- Blood Bank/bloodbank_app/models.py
+++ Blood Bank/bloodbank_app/models.py
@@ -10,4 +10,17 @@ class Bloodbank(models.Model):
 
     def __str__(self):
         return "Distance from {self.location} to {self.destination} is {self.distance} km"
-        
+
+
+class Register(models.Model):
+    name = models.CharField(max_length=150)
+    gender = models.CharField(max_length=200)
+    age = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    bloodgroup = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    passwordr = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
--- Blood Bank/bloodbank_app/views.py
+++ Blood Bank/bloodbank_app/views.py
@@ -16,7 +16,19 @@ def requestsendView(request):
 	return render(request, 'requestsend.html')
 
 def homepageView(request):
-    return render(request, 'login/al/homepage.html')
+
+    name = request.POST.get('name')
+    gender = request.POST.get('gender')
+    age = request.POST.get('age')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    bloodgroup = request.POST.get('bloodgroup')
+    password = request.POST.get('password')
+    passwordr = request.POST.get('passwordr')
+    all = [name, gender, age, email, phone, bloodgroup, password, passwordr]
+
+    return render(request, 'login/al/homepage.html', {'all':all})
 
 def profileView(request):
+
     return render(request, 'al/profile.html')
